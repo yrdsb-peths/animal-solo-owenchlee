70d60aed2ed561ab1cdacd77c9a6bec5511fba65
owen <149855392+netsonceo@users.noreply.github.com>
2025-05-06 18:48:13 -0400

    Reapply "Set background in constructor"
    This reverts commit 47226edd840f43a0bfdf2d57396770aae80745a9.

--- MyWorld.java
+++ MyWorld.java
@@ -28,6 +28,8 @@ public class MyWorld extends World
         addObject(scoreLabel, 50, 50);
         
         createApple();
+        
+        setBackground("images/Background.jpg");
     }
     
     /**
